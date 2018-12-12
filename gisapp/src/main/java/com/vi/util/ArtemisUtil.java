06d0c565046926e8e16919d78768d37a1cfc0234
ddwangw <ddwanglife@163.com>
2018-12-12 11:02:59 +0800

    编码乱码，去除中文，maven install时候会报错

--- gisapp/src/main/java/com/vi/util/ArtemisUtil.java
+++ gisapp/src/main/java/com/vi/util/ArtemisUtil.java
@@ -1,60 +1,31 @@
 package com.vi.util;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.hikvision.artemis.sdk.ArtemisHttpUtil;
 import com.hikvision.artemis.sdk.config.ArtemisConfig;
 
-/**
- * 该Class为测试类, 主要展示常用的接口调用, 这里涉及的接口主要是基础视频常用的接口.
- * 分别会涉及两种接口:
- * 	[1] 分页获取全部监控点数据
- * 	https://open8200.hikvision.com/artemis-portal/document?version=4&docId=306&apiBlock=1
- * 	[2] 根据appKey获取加密协议
- *  https://open8200.hikvision.com/artemis-portal/document?version=4&docId=284&apiBlock=2
- * @author zhangtuo
- *
- */
-public class ArtemisUtil {
 
-	/**
-	 * 请根据自己的appKey和appSecret更换static静态块中的三个参数.
-	 * [1 host]
-	 * 		如果你选择的是同open8200对接,也就是说非现场环境,海康总部的演示环境,host不用修改.默认为open8200.hikvision.com
-	 * 		如果你选择的是同现场环境对接,host要修改为现场环境的ip,网关1.0 时，端口默认为9999.网关2.0 时，端口默认为443.例如:10.33.25.22:9999 或者10.33.25.22:443
-	 * [2 appKey和appSecret]
-	 * 		请按照或得到的appKey和appSecret更改.
-	 * 		
-	 * ps. 如果同海康open8200的演示环境对接成功后,切换到现场环境,请按照现场情况,更换三个参数.
-	 * 
-	 */
+public class ArtemisUtil {
+	private static List<Object> appSecretEffectiveTime = new ArrayList<Object>();
+
 	static {
-		ArtemisConfig.host ="172.16.99.164"; //artemis网关服务器ip端口
-        ArtemisConfig.appKey ="21325930";     //秘钥appkey
-        ArtemisConfig.appSecret ="aQ3LItPftHXaPxVjlErW";//秘钥appSecret
+		ArtemisConfig.host ="172.16.99.164"; 
+        ArtemisConfig.appKey ="21325930";     
+        ArtemisConfig.appSecret ="aQ3LItPftHXaPxVjlErW";
 	}
-	/**
-	 * 能力开放平台的网站路径
-	 * TODO 路径不用修改，就是/artemis
-	 */
+
 	private static final String ARTEMIS_PATH = "/artemis";
 	
-	/**
-	 * [1] 分页获取所有监控点数据调用函数
-	 * @return
-	 */
+
 	public static String callApiGetCameraInfos() {
-		/**
-		 * 	https://open8200.hikvision.com/artemis-portal/document?version=4&docId=306&apiBlock=1
-		 *  根据API文档可以看出来,这是一个GET请求的Rest接口, 而且传入的参数值为queryString, 也就是说明请求参数类似:
-		 *  	http://ip:port/path?a=1&b=2
-		 *  ArtemisHttpUtil工具类提供了doGetArtemis这个函数, 一共四个参数在文档里写明其中的意思.
-		 *  因为接口是https, 所以第一个参数path是个hashmap类型,请put一个key-value, querys为传入的参数.
-		 *  start为起始页, size为每页量.
-		 */
+	
 		String getCamsApi = ARTEMIS_PATH + "/api/common/v1/remoteCameraInfoRestService/findCameraInfoPage";
-		Map<String,String> querys = new HashMap<String,String>();//get请求的查询参数
+		Map<String,String> querys = new HashMap<String,String>();
         querys.put("start", "0");
         querys.put("size", "20");
         Map<String, String> path = new HashMap<String, String>(2){
@@ -66,28 +37,35 @@ public class ArtemisUtil {
 		return result;
 	}
 	
-	/**
-	 * [2] 根据appKey获取加密协议
-	 * @return
-	 */
+	
 	public static String callApiGetSecurity() {
-		/**
-		 * 	https://open8200.hikvision.com/artemis-portal/document?version=4&docId=284&apiBlock=2
-		 *  根据API文档可以看出来,这是一个GET请求的Rest接口,
-		 *  接口中包含了一个Parameter Path,没有queryString的请求参数.
-		 *  接口路径: /api/artemis/v1/agreementService/securityParam/appKey/{appKey}
-		 *  {appKey}是Parameter Path
-		 *  因此, doGetArtemis第二个参数可以为null
-		 *
-		 *  TODO 下面路径末尾的appKey必须修改为static代码块中相同的appKey
-		 */
+		
 		String getSecurityApi = ARTEMIS_PATH + "/api/artemis/v1/agreementService/securityParam/appKey/21325930";
         Map<String, String> path = new HashMap<String, String>(2){
             {
                  put("https://", getSecurityApi);
             }
         };
-        String result =  ArtemisHttpUtil.doGetArtemis(path, null,null,null);
+        String result = "";
+        if(appSecretEffectiveTime.size()>0){
+        	Date lastRequestTime = (Date) appSecretEffectiveTime.get(0);
+        	Date nowTime = new Date();
+        	long diff = nowTime.getTime() - lastRequestTime.getTime();//锟斤拷锟斤拷锟矫碉拷锟侥诧拷值锟斤拷微锟诫级锟斤拷
+            long days = diff / (1000 * 60 * 60 * 24);
+            long hours = (diff-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
+            long minutes = (diff-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
+            if(minutes>=0){
+            	result = ArtemisHttpUtil.doGetArtemis(path, null,null,null);
+            	appSecretEffectiveTime.set(0, nowTime);
+            	appSecretEffectiveTime.set(1, result);
+            }else{
+            	result = (String) appSecretEffectiveTime.get(1);
+            }
+    	}else{
+    		result = ArtemisHttpUtil.doGetArtemis(path, null,null,null);
+    		appSecretEffectiveTime.add(new Date());
+    		appSecretEffectiveTime.add(result);
+    	}
         if("null".equals(result)||null==result) {
         	result = "{\"msg\":\"ERROR\"}";
         }
@@ -95,14 +73,7 @@ public class ArtemisUtil {
 	}
 	
 	public static void main(String[] args) {
-		/**
-		 * 分页获取监控点数据
-		 */
 //		String camsResult = callApiGetCameraInfos();
-		
-		/**
-		 * 根据appKey获取加密协议
-		 */
 		String securityResult = callApiGetSecurity();
 //		System.out.println(camsResult);
 		System.out.println(securityResult);
