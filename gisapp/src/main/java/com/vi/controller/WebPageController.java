bc30af9ef9b01c9a433dd9b2d6838070e4d3cea1
ddwangw <ddwanglife@163.com>
2018-12-12 11:02:06 +0800

    页面模型在打包启动访问的时候无法访问

--- gisapp/src/main/java/com/vi/controller/WebPageController.java
+++ gisapp/src/main/java/com/vi/controller/WebPageController.java
@@ -5,25 +5,22 @@ import java.util.HashMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebPageController {
-	 /**
-     * @param map
-     * @return
-     */
     @RequestMapping("/map")
     public String mapHtml(HashMap<String, Object> map) {
         return "/GraphicIndex";
     }
-    
-    /**
-     * @param map
-     * @return
-     */
     @RequestMapping("/videoTest")
-    public String videoHtml(HashMap<String, Object> map,@RequestParam(name = "videoId") String videoId) {
+    public ModelAndView videoHtml(HashMap<String, Object> map,@RequestParam(name = "videoId") String videoId) {
     	map.put("videoId", videoId);
-        return "/playview";
+//        return "/playview";
+        return  new ModelAndView("playview");
+    }
+    @RequestMapping("/graphicmap")
+    public ModelAndView bankInfo() {
+        return  new ModelAndView("GraphicIndex");
     }
 }
