55d6c99dd490321a9967bdf5a5689ea000e9dfcc
ddwangw <ddwanglife@163.com>
2018-12-13 20:44:17 +0800

    中文字符乱码，删除

--- gisapp/src/main/java/com/vi/util/ArtemisUtil.java
+++ gisapp/src/main/java/com/vi/util/ArtemisUtil.java
@@ -50,7 +50,7 @@ public class ArtemisUtil {
         if(appSecretEffectiveTime.size()>0){
         	Date lastRequestTime = (Date) appSecretEffectiveTime.get(0);
         	Date nowTime = new Date();
-        	long diff = nowTime.getTime() - lastRequestTime.getTime();//�����õ��Ĳ�ֵ��΢�뼶��
+        	long diff = nowTime.getTime() - lastRequestTime.getTime();
             long days = diff / (1000 * 60 * 60 * 24);
             long hours = (diff-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
             long minutes = (diff-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
